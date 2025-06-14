ad1e34368b14fe2286bd97725284df76228110f4
Pei-ju <410173tcgs@gmail.com>
2025-06-14 23:27:43 +0800

    新增Amenity、City、district、Hotel、Image、RoomType、User的entity

---
+++ staytion/src/main/java/com/example/demo/entity/City.java
@@ -0,0 +1,77 @@
+package com.example.demo.entity;
+
+import java.util.List;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "cities")
+public class City {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(name = "cname")
+	private String cname;
+
+	@Column(name = "img_url")
+	private String imgUrl;
+
+	public City() {
+
+	}
+
+	public City(String cname, String imgUrl) {
+		this.cname = cname;
+		this.imgUrl = imgUrl;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getCname() {
+		return cname;
+	}
+
+	public void setCname(String cname) {
+		this.cname = cname;
+	}
+
+	public String getImgUrl() {
+		return imgUrl;
+	}
+
+	public void setImgUrl(String imgUrl) {
+		this.imgUrl = imgUrl;
+	}
+
+//	-------------------------------------
+	@OneToMany(mappedBy = "city")
+	private List<District> districts;
+
+	public List<District> getDistricts() {
+		return districts;
+	}
+
+	public void setDistricts(List<District> districts) {
+		this.districts = districts;
+	}
+
+	// -------------------------------------
+	@Override
+	public String toString() {
+		return "City{" + "id=" + id + ", cname='" + cname + '\'' + ", imgUrl='" + imgUrl + '\'' + '}';
+	}
+
+}
