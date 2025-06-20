8d5b31f81b38240112a3be81af9c6a59909b7582
Arvin <xinyu1008h@gmail.com>
2025-06-20 16:06:57 +0800

    rw暫存

--- src/main/java/com/example/demo/dto/ReviewDTO.java
+++ src/main/java/com/example/demo/dto/ReviewDTO.java
@@ -1,5 +1,81 @@
 package com.example.demo.dto;
 
-public class ReviewDTO {
+import java.time.LocalDateTime;
 
+public class ReviewDTO {
+    private Long orderId;
+    private Integer score;
+    private String comment;
+    private String reply;
+    private String userName;
+    private LocalDateTime createdAt;
+    private Long userId;
+
+    public ReviewDTO(){}
+    public ReviewDTO(Long orderId, Integer score, String comment, String reply,
+                     String userName, LocalDateTime createdAt, Long userId) {
+        this.orderId = orderId;
+        this.score = score;
+        this.comment = comment;
+        this.reply = reply;
+        this.userName = userName;
+        this.createdAt = createdAt;
+        this.userId = userId;
+
+    }
+
+    public Long getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(Long orderId) {
+        this.orderId = orderId;
+    }
+
+    public Integer getScore() {
+        return score;
+    }
+
+    public void setScore(Integer score) {
+        this.score = score;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
+    public String getReply() {
+        return reply;
+    }
+
+    public void setReply(String reply) {
+        this.reply = reply;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
 }
