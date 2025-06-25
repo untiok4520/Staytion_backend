d0172124b44db69e0851f238a68b1944d952304c
Pei-ju <410173tcgs@gmail.com>
2025-06-25 23:24:39 +0800

    新增hotelId及hotelName到ReviewResponse

--- src/main/java/com/example/demo/dto/response/ReviewResponseDto.java
+++ src/main/java/com/example/demo/dto/response/ReviewResponseDto.java
@@ -3,80 +3,101 @@ package com.example.demo.dto.response;
 import java.time.LocalDateTime;
 
 public class ReviewResponseDto {
-    private Long orderId;
-    private Integer score;
-    private String comment;
-    private String reply;
-    private String firstName;
-    private LocalDateTime createdAt;
-    private Long userId;
+	private Long orderId;
+	private Long hotelId;
+	private String hotelName;
+	private Integer score;
+	private String comment;
+	private String reply;
+	private String firstName;
+	private LocalDateTime createdAt;
+	private Long userId;
 
-    public ReviewResponseDto() {}
+	public ReviewResponseDto() {
+	}
 
-    public ReviewResponseDto(Long orderId, Integer score, String comment, String reply,
-                             String firstName, LocalDateTime createdAt, Long userId) {
-        this.orderId = orderId;
-        this.score = score;
-        this.comment = comment;
-        this.reply = reply;
-        this.firstName = firstName;
-        this.createdAt = createdAt;
-        this.userId = userId;
-    }
+	public ReviewResponseDto(Long orderId, Long hotelId, String hotelName, Integer score, String comment, String reply,
+			String firstName, LocalDateTime createdAt, Long userId) {
+		this.orderId = orderId;
+		this.hotelId = hotelId;
+		this.hotelName = hotelName;
+		this.score = score;
+		this.comment = comment;
+		this.reply = reply;
+		this.firstName = firstName;
+		this.createdAt = createdAt;
+		this.userId = userId;
+	}
 
-    public Long getOrderId() {
-        return orderId;
-    }
+	public Long getOrderId() {
+		return orderId;
+	}
 
-    public void setOrderId(Long orderId) {
-        this.orderId = orderId;
-    }
+	public void setOrderId(Long orderId) {
+		this.orderId = orderId;
+	}
 
-    public Integer getScore() {
-        return score;
-    }
+	public Long getHotelId() {
+		return hotelId;
+	}
 
-    public void setScore(Integer score) {
-        this.score = score;
-    }
+	public void setHotelId(Long hotelId) {
+		this.hotelId = hotelId;
+	}
 
-    public String getComment() {
-        return comment;
-    }
+	public String getHotelName() {
+		return hotelName;
+	}
 
-    public void setComment(String comment) {
-        this.comment = comment;
-    }
+	public void setHotelName(String hotelName) {
+		this.hotelName = hotelName;
+	}
 
-    public String getReply() {
-        return reply;
-    }
+	public Integer getScore() {
+		return score;
+	}
 
-    public void setReply(String reply) {
-        this.reply = reply;
-    }
+	public void setScore(Integer score) {
+		this.score = score;
+	}
 
-    public String getFirstName() {
-        return firstName;
-    }
+	public String getComment() {
+		return comment;
+	}
 
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
+	public void setComment(String comment) {
+		this.comment = comment;
+	}
 
-    public LocalDateTime getCreatedAt() {
-        return createdAt;
-    }
+	public String getReply() {
+		return reply;
+	}
 
-    public void setCreatedAt(LocalDateTime createdAt) {
-        this.createdAt = createdAt;
-    }
+	public void setReply(String reply) {
+		this.reply = reply;
+	}
 
-    public Long getUserId() {
-        return userId;
-    }
+	public String getFirstName() {
+		return firstName;
+	}
 
-    public void setUserId(Long userId) {
-        this.userId = userId;
-    }
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public LocalDateTime getCreatedAt() {
+		return createdAt;
+	}
+
+	public void setCreatedAt(LocalDateTime createdAt) {
+		this.createdAt = createdAt;
+	}
+
+	public Long getUserId() {
+		return userId;
+	}
+
+	public void setUserId(Long userId) {
+		this.userId = userId;
+	}
 }
