462ab5af2ea2e04db7d924ffa783a99d8c31342e
untiok4520 <ycchu4520@gmail.com>
2025-06-29 02:00:47 +0800

    修改收藏清單查詢api，串接前端完成

--- src/main/java/com/example/demo/repository/ImageRepository.java
+++ src/main/java/com/example/demo/repository/ImageRepository.java
@@ -1,13 +1,21 @@
 package com.example.demo.repository;
 
-import java.util.List;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
 import com.example.demo.entity.Image;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface ImageRepository extends JpaRepository<Image, Long> {
     List<Image> findByHotelId(Long hotelId);
 
     List<Image> findByHotelIdAndIsCover(Long hotelId, Boolean isCover);
+
+    //查詢飯店首圖
+    @Query("""
+                SELECT img.imgUrl
+                FROM Image img
+                WHERE img.hotel.id = :hotelId AND img.isCover = true
+            """)
+    List<String> findCoverImageByHotelId(Long hotelId);
 }
