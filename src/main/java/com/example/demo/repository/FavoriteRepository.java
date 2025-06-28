9b3fcbd264b25205b2b16dae619fe3e1af7af579
untiok4520 <ycchu4520@gmail.com>
2025-06-29 00:15:37 +0800

    修改收藏清單查詢

--- src/main/java/com/example/demo/repository/FavoriteRepository.java
+++ src/main/java/com/example/demo/repository/FavoriteRepository.java
@@ -1,7 +1,7 @@
 package com.example.demo.repository;
 
+import com.example.demo.dto.FavHotelDto;
 import com.example.demo.entity.Favorite;
-import com.example.demo.entity.Hotel;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -10,16 +10,34 @@ import java.util.List;
 
 public interface FavoriteRepository extends JpaRepository<Favorite, Long> {
     void deleteByUserIdAndHotelId(Long userId, Long hotelId);
+
     boolean existsByUserIdAndHotelId(Long userId, Long hotelId);
 
     @Query("""
-        SELECT f.hotel FROM Favorite f
-        WHERE f.userId = :userId
-        AND (:city IS NULL OR f.hotel.district.city.cname = :city)
-    """)
-    List<Hotel> findFavoriteHotelsByUserIdAndCity(@Param("userId") Long userId, @Param("city") String city);
-
-
-
-
+                SELECT new com.example.demo.dto.FavHotelDto(
+                    h.id,
+                    h.hname,
+                    c.cname,
+                    d.dname,
+                    CAST(COALESCE(ROUND(AVG(rv.score), 1),0) AS double),
+                    CAST(COALESCE(MIN(rt.price), 0) AS double),
+                    COUNT(rv),
+                    img.imgUrl
+                )
+                FROM Favorite f
+                JOIN f.hotel h
+                JOIN h.district d
+                JOIN d.city c
+                LEFT JOIN h.images img ON img.isCover = true
+                LEFT JOIN h.reviews rv
+                LEFT JOIN h.roomTypes rt
+                WHERE f.userId = :userId
+                  AND (:city IS NULL OR c.cname LIKE CONCAT('%', :city, '%'))
+                GROUP BY
+                    h.id, h.hname, c.cname, d.dname, img.imgUrl
+            """)
+    List<FavHotelDto> findFavoritesDtoByUserIdAndCity(
+            @Param("userId") Long userId,
+            @Param("city") String city
+    );
 }
