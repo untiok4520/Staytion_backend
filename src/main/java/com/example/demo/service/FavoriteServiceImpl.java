d3000c19f9454c497b8f0e23a7d6b045b6ef0cd3
untiok4520 <ycchu4520@gmail.com>
2025-06-29 00:15:37 +0800

    修改收藏清單查詢

--- src/main/java/com/example/demo/service/FavoriteServiceImpl.java
+++ src/main/java/com/example/demo/service/FavoriteServiceImpl.java
@@ -3,8 +3,6 @@ package com.example.demo.service;
 import com.example.demo.dto.FavHotelDto;
 import com.example.demo.dto.FavoriteDto;
 import com.example.demo.entity.Favorite;
-import com.example.demo.entity.Hotel;
-import com.example.demo.entity.Image;
 import com.example.demo.repository.FavoriteRepository;
 import com.example.demo.repository.HotelRepository;
 import jakarta.transaction.Transactional;
@@ -13,11 +11,11 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 
 @Service
-public class FavoriteServiceImpl implements FavoriteService{
+public class FavoriteServiceImpl implements FavoriteService {
     private final FavoriteRepository favoriteRepository;
     private final HotelRepository hotelRepository;
 
-    public FavoriteServiceImpl(FavoriteRepository favoriteRepository, HotelRepository hotelRepository){
+    public FavoriteServiceImpl(FavoriteRepository favoriteRepository, HotelRepository hotelRepository) {
         this.favoriteRepository = favoriteRepository;
         this.hotelRepository = hotelRepository;
     }
@@ -46,22 +44,6 @@ public class FavoriteServiceImpl implements FavoriteService{
     //用城市查詢篩選收藏清單
     @Override
     public List<FavHotelDto> getFavoriteHotels(Long userId, String city) {
-        List<Hotel> hotels = favoriteRepository.findFavoriteHotelsByUserIdAndCity(userId, city);
-
-        return hotels.stream()
-                .map(h -> {
-                    String coverUrl = h.getImages().stream()
-                            .filter(Image::getIsCover)
-                            .map(Image::getImgUrl)
-                            .findFirst()
-                            .orElse(null);
-                    return new FavHotelDto(
-                            h.getId(),
-                            h.getHname(),
-                            h.getDistrict().getCity().getCname(),
-                            coverUrl
-                    );
-                })
-                .toList();
+        return favoriteRepository.findFavoritesDtoByUserIdAndCity(userId, city);
     }
 }
