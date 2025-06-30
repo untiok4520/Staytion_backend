bab82fd35e95364ec22caae8f0b38f232bbbd969
Jing-89 <imiss0826@gmail.com>
2025-06-30 15:48:03 +0800

    新增模糊搜尋飯店api

--- src/main/java/com/example/demo/service/HotelService.java
+++ src/main/java/com/example/demo/service/HotelService.java
@@ -5,17 +5,21 @@ import java.util.List;
 import com.example.demo.dto.HotelDetailDTO;
 import com.example.demo.dto.HotelSearchRequest;
 import com.example.demo.dto.HotelSearchResult;
+import com.example.demo.entity.Hotel;
+
 import org.springframework.data.domain.Page;import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.example.demo.projection.HotelProjection;
 import com.example.demo.repository.HotelRepository;
 
-@Service
+
 public interface HotelService {
     List<HotelProjection> getTopHotels();
 
     Page<HotelSearchRequest> searchHotels(HotelSearchResult dto, int page, int size);
 
     HotelDetailDTO getHotelDetail(Long hotelId);
+    
+    List<HotelSearchRequest> searchHotelsByName(String keyword, Long highlightHotelId);
 }
